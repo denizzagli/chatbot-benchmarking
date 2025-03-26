1dc2ee283f9dd07c8b91fd68c3e85c4b41158395
Deniz Zagli <ge74veb@mytum.de>
2025-03-27 00:17:46 +0100

    Add arguments to options

--- frontend_tests/utils/selenium_config.py
+++ frontend_tests/utils/selenium_config.py
@@ -1,19 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from utils.logger import get_logger
-from utils import config
 
 logger = get_logger()
 
 def get_web_driver():
-    logger.info("Cretaion of web driver for Chrome is starting.")
-    
-    options = webdriver.ChromeOptions()
+    logger.info("Creation of web driver for Chrome is starting.")
+
+    options = Options()
     options.add_argument("--headless=new")
+    options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument(config.WINDOW_SIZE)
+    options.add_argument("--disable-extensions")
+    options.add_argument("--window-size=1920,1080")
+
+    import tempfile
+    options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
+
     driver = webdriver.Chrome(options=options)
 
-    logger.info("Cretaion of web driver was ended.")
-    
+    logger.info("Creation of web driver was ended.")
     return driver
