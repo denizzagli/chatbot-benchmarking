6986cfa5309c7c152ac2f49f4055f7dc431a7ad0
Deniz Zagli <ge74veb@mytum.de>
2025-03-27 16:06:38 +0100

    Add response test

---
+++ frontend_tests/utils/selenium_response_text.py
@@ -0,0 +1,37 @@
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from utils.logger import get_logger
+
+logger = get_logger()
+
+def get_response_time(driver, message):
+    logger.info("Response timer is starting. Message: " + message)
+    
+    chat_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//input[@type='text']")))
+
+    send_button = driver.find_element(By.XPATH, "//button[contains(@class, 'ant-btn-primary')]")
+
+    chat_input.send_keys(message)
+
+    WebDriverWait(driver, 10).until(lambda d: send_button.is_enabled())
+
+    start_time = time.time()
+
+    send_button.click()
+
+    initial_message_count = len(driver.find_elements(By.XPATH, "//div[contains(@class, 'message-container')]//span[contains(@class, 'message-text')]"))
+
+    WebDriverWait(driver, 30).until(
+        lambda d: len(d.find_elements(By.XPATH, "//div[contains(@class, 'message-container')]//span[contains(@class, 'message-text')]")) > initial_message_count
+    )
+
+    messages = driver.find_elements(By.XPATH, "//div[contains(@class, 'message-container')]//span[contains(@class, 'message-text')]")
+
+    response_text = messages[-1].text
+
+    logger.info("Response timer was ended. " + f"Response of the chatbot: {response_text}")
+    
+    return response_time
